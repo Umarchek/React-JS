301ec3bd1252c1444775602cf4c5b341d5c5dbd3
UZBase <sharofiddinov.07@mail.ru>
2021-12-07 16:31:00 +0300

    added style

--- src/Components/PrimaryButtons.js
+++ src/Components/PrimaryButtons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-function PrimaryButtons({name}) {
+function PrimaryButtons({ name }) {
     return (
         <ButtonStyled>
             {name}
@@ -8,6 +8,14 @@ function PrimaryButtons({name}) {
     )
 }
 const ButtonStyled = styled.button`
-
+background-color: var(--accent-pink);
+padding: .7rem 2rem;
+font-family: inherit;
+font-size: inherit;
+color: inherit;
+border-radius: 20px;
+outline: none;
+border: none;
+cursor: pointer;
 `;
 export default PrimaryButtons
