b94fa57d707d67a4f8378ba35f083150eedcac0f
UZBase <sharofiddinov.07@mail.ru>
2021-12-08 17:47:33 +0300

    Create ChartSection.js

---
+++ src/Components/ChartSection.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ChartSection() {
+    return (
+        <ChartStyled>
+            <div className="chart-left">
+        
+            </div>
+            <div className="chart-right">
+
+            </div>
+        </ChartStyled>
+    )
+}
+
+const ChartStyled = styled.section`
+`
+
+export default ChartSection
