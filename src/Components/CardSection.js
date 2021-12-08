d55b1cd30d68c526c67424c46006e78a2d904aea
UZBase <sharofiddinov.07@mail.ru>
2021-12-08 16:43:41 +0300

    ...

--- src/Components/CardSection.js
+++ src/Components/CardSection.js
@@ -6,20 +6,42 @@ function CardSection() {
     return (
         <CardSectionStyled>
             <InnerLayout>
-                <div className="card-left">
-                    <h2 className="secondary-headeing">
-                        One card for all your payments
-                    </h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci quisquam tempora minus at dicta impedit nemo asperiores beatae quis. Libero iusto labore placeat quam consequuntur!
-                    </p>
+                <div className="card-container">
+                    <div className="card-left">
+                        <h2 className="secondary-heading">
+                            One card for all your payments
+                        </h2>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt reprehenderit doloremque quia? Odit illo, tempore quod vero exercitationem, 
+                            voluptatum laudantium quo harum, adipisci tenetur eum.
+                        </p>
+                    </div>
+                    <div className="card-right">
+                        <img src={card} alt="" />
+                    </div>
                 </div>
-                <div className="card-right">
-                    <img src={card} alt="" /></div>
             </InnerLayout>
         </CardSectionStyled>
     )
 }
 const CardSectionStyled = styled.section`
-`
+    .card-container{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        @media screen and (max-width: 845px){
+            grid-template-columns: repeat(1, 1fr);
+        }
+        .card-right{
+            display: flex;
+            justify-content: flex-end;
+            img{filter: drop-shadow(0px 50px 100px rgba(22, 25, 79, 0.15));}
+        }
+        .card-left{
+            p{
+                padding: 1rem 0;
+            }
+        }
+    }
+`;
+
 export default CardSection
