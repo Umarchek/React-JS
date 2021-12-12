a596e8de588a25e7e11172ba4e6d08f2035c55bd
UZBase <sharofiddinov.07@mail.ru>
2021-12-12 10:08:16 +0300

    Update FAQSection.js

--- src/Components/FAQSection.js
+++ src/Components/FAQSection.js
@@ -6,11 +6,28 @@ function FAQSection() {
         <FaqStyled>
             <InnerLayout>
                 <h3 className="small-heading">Frequently <span>asked questions</span></h3>
-                <p className='c-pare'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta reprehenderit facilis quibusdam quos excepturi impedit soluta perspiciatis. Porro error mollitia molestias est, labore, odit nulla, facilis iure tenetur accusantium consequatur inventore ipsum!</p>
+                <p className='c-para'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta reprehenderit facilis quibusdam quos excepturi impedit soluta perspiciatis. Porro error mollitia molestias est, labore, odit nulla, facilis iure tenetur accusantium consequatur inventore ipsum!</p>
             </InnerLayout>
         </FaqStyled>
     )
 }
 const FaqStyled = styled.section`
-`
+    .c-para{
+        width: 60%;
+        margin: 0 auto;
+    }
+    .questions-con{
+        padding-top: 4rem;
+    }
+    .lines{
+        position: absolute;
+        left: 0;
+        top: 300%;
+        width: 100%;
+        z-index: -1;
+        img{
+            width: 100%;
+        }
+    }
+`;
 export default FAQSection
